df43a1758c31a436c01e85fbf5a79d2378d835fe
SoulFirexD <vini.torresc@gmail.com>
2017-05-29 22:42:22 -0300

    aula 3 app pizzaria final da aula

--- app/src/main/java/br/com/vini/pizzaria/LoginActivity.java
+++ app/src/main/java/br/com/vini/pizzaria/LoginActivity.java
@@ -1,13 +1,43 @@
 package br.com.vini.pizzaria;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText etLogin;
+    private EditText etSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etLogin = (EditText) findViewById(R.id.tilLogin);
+        etSenha = (EditText) findViewById(R.id.tilSenha);
+    }
+
+    public void logar(View view) {
+//Recuperar os valores digitados
+        String login = etLogin.getText().toString();
+        String senha = etSenha.getText().toString();
+//Valida o usuário e senha
+        if (login.equals("FIAP") && senha.equals("123")) {
+//Mudar de tela Tela de Destino
+            Intent intent = new Intent(this, MainActivity.class);
+//Passar uma valor para a outra tela
+            intent.putExtra("usuario", login);
+//Iniciar a outra tela
+            startActivity(intent);
+            finish();
+        } else {
+//Login ou senha inválidos
+            Toast.makeText(this, "Usuário ou senha inválidos",
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 }
